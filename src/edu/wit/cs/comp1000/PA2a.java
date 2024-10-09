7421c045ea9c0606f70d61f83d27e00764cb110f
Angelina Lin <lina7@wit.edu>
2024-10-09 17:48:24 -0400

    Programming Assignment 2

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,51 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		
+		int N1,N2,N3,N4,N5;
+		double sum=0, positive_sum=0,non_positive_sum=0;
+		double average=0, positive_average=0, non_positive_average=0;
+		int positive_numbers=0 ,non_positive_numbers=0;
+		int i = 0;
+		System.out.printf("Enter five whole numbers:");
+		
+		while (i<5) {
+			N1=input.nextInt(); 
+			i = i+1;
+			sum = sum+N1;
+			average = sum/i;
+			if (N1>=0) {
+				positive_numbers = positive_numbers+1;
+				positive_sum = positive_sum+N1;
+				positive_average = positive_sum/positive_numbers;
+			}
+			else {
+				non_positive_numbers = non_positive_numbers+1;
+				non_positive_sum = non_positive_sum+N1;
+				non_positive_average = non_positive_sum/non_positive_numbers;
+			}
+				
+		}
+			
+		System.out.printf("The sum of the %d positive numbers: %.0f%n",positive_numbers,positive_sum);
+		System.out.printf("The sum of the %d non-positive numbers: %.0f%n",non_positive_numbers,non_positive_sum);
+		System.out.printf("The sum of the 5 numbers: %.0f%n", sum);
+		
+		System.out.printf("The average of the %d positive numbers: %.2f%n",positive_numbers,positive_average);
+		System.out.printf("The average of the %d non-positive numbers: %.2f%n",non_positive_numbers,non_positive_average);
+		System.out.printf("The average of the 5 numbers: %.2f%n", average);
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,45 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		Scanner input = new Scanner (System.in);
+		
+		int a,b,c,d;
+		double root,root2,root3;
+		
+		System.out.printf("Enter a b c: ");
+		
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		
+		d = (b*b)-(4*a*c);
+		
+		
+		if (d<0) {
+			System.out.printf("Roots: imaginary");	
+		}
+		else if (d==0) {
+			root = -b/2*a;
+			System.out.printf("Root: %.2f%n", root);
+		}
+		else {
+			root2 = (-b+ Math.sqrt(d))/2*a;
+			root3 = (-b- Math.sqrt(d))/2*a;
+		
+			if (root2<root3) {
+				System.out.printf("Roots: %.2f, %.2f%n", root2,root3);
+			}
+			else {
+				System.out.printf("Roots: %.2f, %.2f%n", root3,root2);
+			}
+		}	
 	}
+	
+	
 
 }
